c1d88786aa91f0c8ea3729675aef648b11d3aff0
S2 <s@ireoo.com>
2016-11-16 04:01:23 +0800

    更新程序

--- main.js
+++ main.js
@@ -5,8 +5,8 @@ var needle = require('needle'),
     cheerio = require('cheerio');
 
 needle.defaults({
-    open_timeout: 5000,
-    user_agent: 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:42.0) Gecko/20100101 Firefox/42.0'
+    open_timeout: 60 * 1000,
+    user_agent: 'iGather.npm'
 });
 
 function jiexi(data) {
